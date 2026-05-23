092d3d0e1914ac303fb05831545bc36a88459349
RaminIsmayilsoy <raminismayilsoy@gmail.com>
2026-05-24 00:01:54 +0400

    all

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -1,23 +1,27 @@
 #!/usr/bin/env ruby
 require 'net/http'
-require 'uri'
 require 'json'
+require 'uri'
 
-def post_request(url, body_params = {})
+def post_request(url, body_params)
   uri = URI.parse(url)
+  request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
+  request.body = body_params.to_json
 
-  # Create an HTTP POST request
-  request = Net::HTTP::Post.new(uri)
-  request.content_type = 'application/json'
-  request.body = body_params.to_json unless body_params.empty?
-
-  # Initialize the HTTP session
-  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+  response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
     http.request(request)
   end
 
-  # Print response status and body
   puts "Response status: #{response.code} #{response.message}"
-  puts "Response body:"
-  puts JSON.pretty_generate(JSON.parse(response.body))
+  begin
+    json_body = JSON.parse(response.body)
+    if json_body.empty?
+      puts "Response body:\n{}"
+    else
+      puts "Response body:"
+      puts JSON.pretty_generate(json_body)
+    end
+  rescue JSON::ParserError
+    puts "Response body:\n#{response.body}"
+  end
 end
